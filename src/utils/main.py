1b38fe0e1156b6ab7f2b3e87d69fd8fd811ee9b7
RiweKudem <261874853+RiweKudem@users.noreply.github.com>
2025-11-16 12:08:45 +0000

    Added user endpoint with create functionality

---
+++ src/utils/main.py
@@ -0,0 +1,33 @@
+import os
+import logging
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'name': user.name, 'email': user.email} for user in users])
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    user = User(name=data['name'], email=data['email'])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'id': user.id, 'name': user.name, 'email': user.email})
+
+if __name__ == '__main__':
+    if not os.path.exists('database.db'):
+        with app.app_context():
+            db.create_all()
+    app.run(debug=True)
